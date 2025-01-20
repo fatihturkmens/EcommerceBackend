9934427266a6576fab96a80415203a9991a44fc1
Fatih <fatihturkmens@hotmail.com>
2025-01-21 00:30:35 +0300

    Update CustomerController.java

--- src/main/java/com/example/ecommerceproject/controller/CustomerController.java
+++ src/main/java/com/example/ecommerceproject/controller/CustomerController.java
@@ -17,8 +17,31 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-    public ResponseEntity<String> addCustomer(@RequestBody @Valid Customer customer){   // yeni bir müşteri ekleme
-        String added=customerService.addCustomer(customer);
-        return new ResponseEntity<>(added,HttpStatus.CREATED);
+   @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long id){
+       Product product =  productService.getProduct(id);
+       return new ResponseEntity<>(product, HttpStatus.OK);
+
+    }
+    @PostMapping("/ekle")
+    public ResponseEntity<String> createProduct(@RequestBody @Valid Product product){
+       String created= productService.createProduct(product);
+       return new ResponseEntity<>(created,HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<String> updateProduct(@RequestBody @Valid Product product){
+        String updated= productService.updateProduct(product);
+        return new ResponseEntity<>(updated,HttpStatus.CREATED);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id){
+        String deleted = productService.deleteProduct(id);
+        return new ResponseEntity<>(deleted,HttpStatus.OK);
+
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<Product>> allProducts(){
+        return new ResponseEntity<>(productService.productList(),HttpStatus.OK);
     }
 }
