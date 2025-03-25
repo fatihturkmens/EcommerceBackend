d5b2d6875c70a613646f8dc488cb0635068b2580
Fatih <fatihturkmens@hotmail.com>
2025-03-25 16:01:27 +0300

    Update Cart.java

--- src/main/java/com/example/ecommerceproject/model/Cart.java
+++ src/main/java/com/example/ecommerceproject/model/Cart.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Cart extends BaseEntity {
 
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "customer_id",referencedColumnName = "id")
     private Customer customer;
 
